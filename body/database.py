d3f5184a58ca9944dc728eb2044ce0430ab4e9ff
mikotest4 <fakealok756@gmail.com>
2025-07-14 19:35:23 +0530

    Update database.py

--- body/database.py
+++ body/database.py
@@ -1,15 +1,17 @@
 import motor.motor_asyncio
 from info import *
+import random
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
 db = client.captions_with_chnl
 chnl_ids = db.chnl_ids
 users = db.users
+random_captions = db.random_captions
 
+# Original Functions
 async def addCap(chnl_id, caption):
     dets = {"chnl_id": chnl_id, "caption": caption}
     await chnl_ids.insert_one(dets)
-
 
 async def updateCap(chnl_id, caption):
     await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
@@ -31,3 +33,56 @@ async def getid():
 
 async def delete(id):
     await users.delete_one(id)
+
+# New Random Caption Functions
+async def add_random_caption(caption_text):
+    """Add a new random caption to database"""
+    caption_data = {"caption": caption_text}
+    await random_captions.insert_one(caption_data)
+
+async def get_random_caption():
+    """Get a random caption from database"""
+    try:
+        total_captions = await random_captions.count_documents({})
+        if total_captions == 0:
+            return None
+        
+        # Get random number between 0 and total_captions-1
+        random_skip = random.randint(0, total_captions - 1)
+        
+        # Get random caption
+        cursor = random_captions.find({}).skip(random_skip).limit(1)
+        random_caption_doc = await cursor.to_list(length=1)
+        
+        if random_caption_doc:
+            return random_caption_doc[0]['caption']
+        else:
+            return None
+    except Exception as e:
+        print(f"Error getting random caption: {e}")
+        return None
+
+async def delete_random_caption(caption_id):
+    """Delete a random caption by ID"""
+    from bson import ObjectId
+    try:
+        await random_captions.delete_one({"_id": ObjectId(caption_id)})
+        return True
+    except:
+        return False
+
+async def get_all_random_captions():
+    """Get all random captions for listing"""
+    captions = []
+    async for caption in random_captions.find({}):
+        captions.append(caption)
+    return captions
+
+async def total_random_captions():
+    """Get total count of random captions"""
+    count = await random_captions.count_documents({})
+    return count
+
+async def clear_all_random_captions():
+    """Clear all random captions"""
+    await random_captions.delete_many({})
